c30736decc3e3f689ab02fee89ab851be5420347
Kuntal Joshi <kuntalce@gmail.com>
2013-07-16 12:45:15 -0700

    FileDetail interface

---
+++ DBoxInterface/src/dBox/FileDetail.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dBox;
+
+import java.io.File;
+
+/**
+ *
+ * @author Kuntal
+ */
+public class FileDetail
+{
+
+    private File synchFile;
+    private String fileHash;
+}
