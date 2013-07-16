8277946377c1e9a6eb4e0cb919777f867788a471
prablsharma <cena_prabal@yahoo.co.in>
2013-07-16 13:10:27 -0700

    getter setter FileDetails

--- DBoxInterface/src/dBox/FileDetail.java
+++ DBoxInterface/src/dBox/FileDetail.java
@@ -15,4 +15,43 @@ public class FileDetail
 
     private File synchFile;
     private String fileHash;
+
+    public FileDetail(File file, String hash)
+    {
+        this.synchFile = file;
+        this.fileHash = hash;
+
+    }
+
+    /**
+     * @return the synchFile
+     */
+    public File getSynchFile()
+    {
+        return synchFile;
+    }
+
+    /**
+     * @param synchFile the synchFile to set
+     */
+    public void setSynchFile(File synchFile)
+    {
+        this.synchFile = synchFile;
+    }
+
+    /**
+     * @return the fileHash
+     */
+    public String getFileHash()
+    {
+        return fileHash;
+    }
+
+    /**
+     * @param fileHash the fileHash to set
+     */
+    public void setFileHash(String fileHash)
+    {
+        this.fileHash = fileHash;
+    }
 }
